b4d042bd8e0093f5c9d6a6c4600618c1a16678b0
Cameron Harris <cameroniharris1@gmail.com>
2015-10-30 14:12:07 +0000

    Fixed Picture Upload

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,4 +1,4 @@
-class PhotoController < ApplicationController
+class PhotosController < ApplicationController
 	before_action :authenticate_user!
 	def create
 		@place = Place.find(params[:place_id])
